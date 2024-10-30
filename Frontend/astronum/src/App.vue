3a0b5cf9044e84e7de1a2e177dfe611a2f1d6ee9
Victoria Ines <inessavalos14@gmail.com>
2024-10-30 20:42:06 -0300

    proyecto base sin componentes o estilos - instalacion vuejs - tailwind css

---
+++ Frontend/astronum/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">
+    AstroNum
+  </h1>
+</template>
