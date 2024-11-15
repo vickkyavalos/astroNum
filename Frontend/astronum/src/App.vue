9accc15eb94cfc2c2ab99e0f2e01b4aeb408ffe1
Victoria Ines <inessavalos14@gmail.com>
2024-11-15 18:19:02 -0300

    creacion de proyecto angular - componentes carpeta shared

--- Frontend/astronum/src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
-<template>
-  <h1 class="text-3xl font-bold underline">
-    AstroNum
-  </h1>
-</template>
