6e44ed9606b736eae41c5f46831f65c2a6c52be0
Victoria Ines <inessavalos14@gmail.com>
2025-01-15 17:53:42 -0300

    Co-authored-by: Gabr!ela. <gabrielasan07@gmail.com>

---
+++ Frontend/static/js/scripts.js
@@ -0,0 +1,19 @@
+// para los botones, traer by id y agregar evento click que redirija
+//#btn1, #btn2 ,#btn3
+
+//botones
+function irAstrologia(){
+    let boton1 = document.getElementById('btn1') //trae el elemento por el id 
+    boton1 = window.location.href = './../templates/astrologia.html'
+}
+function irAstrologia(){
+    let boton2 = document.getElementById('btn2') //trae el elemento por el id 
+    boton1 = window.location.href = './../templates/astrologia.html'
+}
+function irAstrologia(){
+    let boton3 = document.getElementById('btn3') //trae el elemento por el id 
+    boton1 = window.location.href = './../templates/astrologia.html'
+}
+
+
+
